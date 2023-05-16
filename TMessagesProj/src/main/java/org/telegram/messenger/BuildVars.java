8d48fe3f742bc976a7221bb134debbdc8d4bf9c1
ImOnlyFire <ivancardaropoli77@gmail.com>
2023-05-16 21:12:54 +0200

    Version bump

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -42,8 +42,8 @@ public class BuildVars {
     // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
     public static boolean IS_BILLING_UNAVAILABLE = StoreUtils.isFromPlayStore();
 
-    public static String YUKI_VERSION_STRING = "3.1.8";
-    public static String YUKI_VERSION = "3320";
+    public static String YUKI_VERSION_STRING = "3.1.9";
+    public static String YUKI_VERSION = "3322";
 
     static {
         APP_ID = Extra.APP_ID;
